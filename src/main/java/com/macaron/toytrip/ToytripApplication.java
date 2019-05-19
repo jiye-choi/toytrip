beb0a5119986c155da796e8570b16d95a09d0b7c
jiye <jiye@jiyeui-MacBook-Pro.local>
2019-05-19 17:13:19 +0900

    (공통) 최초 push

---
+++ src/main/java/com/macaron/toytrip/ToytripApplication.java
@@ -0,0 +1,13 @@
+package com.macaron.toytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToytripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToytripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/macaron/toytrip/ToytripApplicationTests.java
@@ -0,0 +1,16 @@
+package com.macaron.toytrip;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToytripApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
