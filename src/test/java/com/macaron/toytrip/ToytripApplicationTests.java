bad6429834c4c3a3a37d17d774807965457d264c
leejaehyuk <nespot2@gmail.com>
2019-05-26 18:06:26 +0900

    test case 로직 추가!

--- src/test/java/com/macaron/toytrip/ToytripApplicationTests.java
+++ src/test/java/com/macaron/toytrip/ToytripApplicationTests.java
@@ -1,16 +1,33 @@
 package com.macaron.toytrip;
 
+import com.macaron.toytrip.dto.AirPlaneInfo;
+import com.macaron.toytrip.service.AirPlaneService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ToytripApplication.class)
 public class ToytripApplicationTests {
+
+
+    @Autowired
+    private AirPlaneService airPlaneService;
+
 
     @Test
     public void contextLoads() {
+
+        final List<AirPlaneInfo> res = airPlaneService.res();
+
+        for (AirPlaneInfo re : res) {
+            System.out.println(re);
+        }
+
     }
 
 }
